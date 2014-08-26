e7acc43578f7dd8e124ef276c4528fc2abf7244e
Chris Warburton <chriswarbo@gmail.com>
2014-08-26 16:47:15 +0100

    Improved hill-climbing

--- essays/optimisation/hillclimbing.md
+++ essays/optimisation/hillclimbing.md
@@ -10,10 +10,6 @@ Each (x, y) point in the square is a solution, and our goal is to find the best 
 <div id="hill_playfield" style="width: 500px; height: 500px;"></div>
 <form action="#" type="get">
 <div>
-  <input type="range" name="_" id="hill_number" min="0" max="10" value="0" style="width: 500px;" />
-  <label for="hill_number">Number of searches:</label>&nbsp;&nbsp;<a id="hill_number_display"></a>
-</div>
-<div>
   <span>Current fitness:</label>&nbsp;&nbsp;<a id="hill_fitness_display"></a>
 </div>
 <div>
@@ -26,6 +22,6 @@ Each (x, y) point in the square is a solution, and our goal is to find the best 
 <script src="/js/underscore.js"></script>
 <script src="/js/optimisation/hill.js"></script>
 
-Moving the "Number of searches" slider will add and remove instances of the search. All of them are running the same code, but their random angles will be different (since they're, um, random). The "Step size" tells the searches how far to move from their current location when they take a step; low values mean that not much of the space gets explored, high values mean that nearby solutions get missed. We don't need a "Desired fitness" slider this time, since the search will stop once it can't find a better solution. We have no idea if the solution it finds will be the best one though; welcome to the [halting problem](http://en.wikipedia.org/wiki/Halting_problem)!
+Click the box to start the search. The "Step size" tells the search how far to move from its current location when taking a step; low values mean that not much of the space gets explored, high values mean that nearby solutions may get missed. Since the search will only move to points with higher fitness, there's no need for a separate "best so-far" marker.
 
-Whilst hill climbing is quite a simple search algorithm, it can give reasonable enough results for simple, smooth search spaces (ie. where it can't get stuck easily). For problems which have a bumpy search space, hill climbing will get stuck at the top of whichever bump it started on. Since hill-climbing will never sacrifice its current fitness, which makes it get stuck, this does give it the nice property of being an [i]anytime algorithm[/i], which means we can stop it at any point and be sure that we're given the best solution found so far. If the algorithm were to cross the valleys between the peaks, then it wouldn't get stuck; however, we may end up stopping it while it's in the middle of a valley, and thus our final solution would be terrible.
+Whilst hill climbing is quite a simple search algorithm, it can give reasonable enough results for simple, smooth search spaces (ie. where it can't get stuck easily). For problems which have a bumpy search space, hill climbing will get stuck at the top of whichever bump it started on.
--- js/optimisation/hill.js
+++ js/optimisation/hill.js
@@ -1,117 +1,116 @@
-window.hill_searchers = [];
-window.hill_lines = [];
-window.gradient = [];
+(function() {
+  var hill_searchers = [];
+  var hill_lines     = [];
+  var gradient       = [];
+  var fitness        = 0;
+  var length;
+  var w;
+  var h;
 
-$(function() {
-	$('#hill_playfield').svg({onLoad: hill_init});
-	$('#hill_number').change(function(){
-	// Set the fitness to -1 when we remove a searcher
-		var new_n = Math.round($('#hill_number').val());
-		$('#hill_number_display').text(new_n+"");
-		while (new_n > window.hill_searchers.length) {
-			window.hill_searchers.push(1);
-			window.setTimeout(function(){
-				hill_step(
-					Math.round(Math.random() * $('#hill_playfield').width()),
-					Math.round(Math.random() * $('#hill_playfield').height()),
-					window.hill_searchers.length - 1
-				);
-			}, 10);
-		}
-		while (new_n < window.hill_searchers.length) {
-			window.hill_searchers.pop();
-		}
-	});
-	$('#hill_number').change();
+  $(function() {
+      w = $('#hill_playfield').width();
+      h = $('#hill_playfield').height();
+      $('#hill_playfield').svg({onLoad: hill_init});
+      $('#hill_playfield').click(
+        function() {
+          $('#hill_step').change();
+          setTimeout(function() {
+                       hill_step(Math.round(Math.random() * w),
+                                 Math.round(Math.random() * h));
+                     }, 10);
+        });
 
-	$('#hill_fitness_display').text(0);
+      $('#hill_fitness_display').text(Math.round(fitness));
 
-	$('#hill_step').change(function(){
-		$('#hill_step_display').text(Math.round($('#hill_step').val()));
-	});
-	$('#hill_step').change();
-});
+      $('#hill_step').change(function() {
+                               length = $('#hill_step').val();
+                               $('#hill_step_display').text(Math.round(length));
+                             });
+      $('#hill_step').change();
+    });
 
-hill_init = function(svg) {
-	_.times(Math.round(Math.random() * 100 + 1), function(){
-		var l = Math.random();
-		var r = Math.random();
-		var t = Math.random();
-		var b = Math.random();
-		var temp;
-		if (l > r) {
-			temp = l;
-			l = r;
-			r = temp;
-		}
-		if (t > b) {
-			temp = t;
-			t = b;
-			b = temp;
-		}
-		window.gradient.push(function(x, y, draw){
-			var w = $('#hill_playfield').width();
-			var h = $('#hill_playfield').height();
-			if (draw) {
-				$('#hill_playfield').svg('get').rect(l*w, t*h, (r-l)*w, (b-t)*h, {'fill': 'white', 'fill-opacity': 0.02});
-			}
-			else {
-				if ((l < x / w) && (r > x / w) && (t < y / h) && (b > y / h)) {
-					return 1;
-				}
-				return 0;
-			}
-		});
-	});
-	$('#hill_playfield').svg('get').rect(0,0,$('#hill_playfield').width(),$('#hill_playfield').height(),{'fill': 'black'});
-	_.each(window.gradient, function(a){a(0, 0, true);});
-}
+  function hill_init(svg) {
+    _.times(Math.round(Math.random() * 100 + 1),
+            function() {
+              var l = Math.random();
+              var r = Math.random();
+              var t = Math.random();
+              var b = Math.random();
+              var temp;
+              if (l > r) {
+                temp = l;
+                l    = r;
+                r    = temp;
+              }
+              if (t > b) {
+                temp = t;
+                t    = b;
+                b    = temp;
+              }
+              gradient.push(function(x, y, draw){
+                              if (draw) {
+                                $('#hill_playfield').svg('get')
+                                                    .rect(    l*w,     t*h,
+                                                          (r-l)*w, (b-t)*h,
+                                                          {'fill': 'white',
+                                                           'fill-opacity': 0.02});
+                              }
+                              else {
+                                if ((l < x / w) && (r > x / w) &&
+                                    (t < y / h) && (b > y / h)) {
+                                  return 1;
+                                }
+                                return 0;
+                              }
+                            });
+            });
+    $('#hill_playfield').svg('get')
+                        .rect(0, 0, w, h, {'fill': 'black'});
+    _.each(gradient, function(a){a(0, 0, true);});
+  }
 
-fitn = function(x, y) {
-  return _.reduce(_.map(window.gradient, function(a){ return a(x, y, false); }), function(a,b) { return a + b; });
-};
+  function fitn(x, y) {
+    return _.reduce(_.map(gradient,
+                          function(a){ return a(x, y, false); }),
+                    function(a,b) { return a + b; });
+  }
 
-hill_step = function(x, y, n) {
-	if (n + 1 > window.hill_searchers.length) {
-		while (window.hill_lines[n].length > 0) {
-			$('#hill_playfield').svg('get').remove(window.hill_lines[n].shift());
-		}
-		return;
-	}
-	var length = $('#hill_step').val();
-	var angle = Math.random() * 2 * Math.PI;
-	var new_x = x + (length * Math.cos(angle));
-	var new_y = y + (length * Math.sin(angle));
-	var w = $('#hill_playfield').width();
-	var h = $('#hill_playfield').height();
-	if ((new_x < 0) || (new_x >= w) || (new_y < 0) || (new_y >= h)) {
-		if (new_x < 0) { new_x = new_x + w - 1; }
-		if (new_x >= w) { new_x = new_x - w + 1; }
-		if (new_y < 0) { new_y = new_y + h - 1; }
-		if (new_y >= h) { new_y = new_y - h + 1; }
-	}
-	else {
-		var old_fit = fitn(x, y);
-		var new_fit = fitn(new_x, new_y);
+  function hill_step(x, y) {
+    var angle = Math.random() * 2 * Math.PI;
+    var new_x = x + (length * Math.cos(angle));
+    var new_y = y + (length * Math.sin(angle));
+    if ((new_x < 0) || (new_x >= w) ||
+        (new_y < 0) || (new_y >= h)) {
+      if (new_x <  0) { new_x = new_x + w - 1; }
+      if (new_x >= w) { new_x = new_x - w + 1; }
+      if (new_y <  0) { new_y = new_y + h - 1; }
+      if (new_y >= h) { new_y = new_y - h + 1; }
+    }
+    else {
+      var old_fit = fitn(x, y);
+      var new_fit = fitn(new_x, new_y);
 
-		while (window.hill_lines.length < n + 1) { window.hill_lines.push([]); }
-		var cols = ['white', 'yellow', 'lime', 'green', 'cyan', 'blue', 'purple'];
-		window.hill_lines[n].push($('#hill_playfield').svg('get').line(x, y, new_x, new_y, {stroke: cols[n % cols.length], 'stroke-width': 1}));
+      hill_lines.push($('#hill_playfield').svg('get')
+                                          .line(    x,     y,
+                                                new_x, new_y,
+                                                {'stroke': 'white',
+                                                 'stroke-width': 1}));
 
-		if (new_fit < old_fit) {
-			new_x = x;
-			new_y = y;
-			new_fit = old_fit;
-		}
+      if (new_fit < old_fit) {
+        new_x = x;
+        new_y = y;
+        new_fit = old_fit;
+      }
 
-		// Find the fitness of this point
-		// Only update if it's more than the current value
-		$('#hill_fitness_display').text(new_fit+"");
+      // Find the fitness of this point
+      // Only update if it's more than the current value
+      $('#hill_fitness_display').text(new_fit+"");
 
-		// Clean up old lines
-		while (window.hill_lines[n].length > 10) {
-			$('#hill_playfield').svg('get').remove(window.hill_lines[n].shift());
-		}
-	}
-	window.setTimeout(function(){ hill_step(new_x, new_y, n); }, 10);
-}
+      // Clean up old lines
+      while (hill_lines.length > 10) {
+        $('#hill_playfield').svg('get').remove(hill_lines.shift());
+      }
+    }
+    setTimeout(function(){ hill_step(new_x, new_y); }, 10);
+  }
+}());
