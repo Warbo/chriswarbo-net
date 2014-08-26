2a5a3ac7fdc78eb4446735b009b57d09cb36d605
Chris Warburton <chriswarbo@gmail.com>
2014-08-26 09:07:54 +0100

    Improved random walk demo

--- essays/optimisation/randomwalk.md
+++ essays/optimisation/randomwalk.md
@@ -5,18 +5,10 @@ Random walks are slightly more complex search algorithms than enumeration. Rathe
 
 In this example, like with enumeration, the fitness of a particular location is shown by how light the grey colour is. This is randomly generated when the page loads.
 
-Each (x, y) point in the square is a solution, and our goal is to find the best (lightest). We start in the centre and move a set distance at a random angle at each step. Like the enumeration method, I've made the edges wrap around. There's no need for a trick like using irrational angles this time, since we're not imposing an order on the search space. This is an advantage of random walks, since they can be dumped on to any problem without any messing around like  craft an exhaustive enumeration. The disadvantage, as you may notice, is that the same (or nearly the same) solutions are checked again and again!
+Each (x, y) point in the square is a solution, and our goal is to find the best (lightest). We start in the centre and move a set distance at a random angle at each step. Like the enumeration method, I've made the edges wrap around. There's no need for tricks in the way we represent the search space, since we're not imposing a fixed order on the points. This is an advantage of random walks, since they can be dumped on to any problem without any messing around like crafting an exhaustive enumeration. The disadvantage, as you may notice, is that the same (or nearly the same) solutions are checked again and again!
 
 <div id="walk_playfield" style="width: 500px; height: 500px;"></div>
 <form action="#" type="get">
-<div>
-  <input type="range" name="_" id="walk_number" min="0" max="10" value="0" style="width: 500px;" />
-  <label for="walk_number">Number of searches:</label>&nbsp;&nbsp;<a id="walk_number_display"></a>
-</div>
-<div>
-  <input type="range" name="_" id="walk_fitness" min="0" max="100" value="10" style="width:500px;" />
-  <label for="walk_fitness">Desired fitness:</label>&nbsp;&nbsp;<a id="walk_fitness_display"></a>
-</div>
 <div>
   <input type="range" name="_" id="walk_step" min="1" max="10" value="2" style="width: 500px;" />
   <label for="walk_step">Step size:</label>&nbsp;&nbsp;<a id="walk_step_display"></a>
@@ -27,6 +19,6 @@ Each (x, y) point in the square is a solution, and our goal is to find the best 
 <script src="/js/underscore.js"></script>
 <script src="/js/optimisation/walk.js"></script>
 
-Moving the "Number of searches" slider will add and remove instances of the search. All of them are running the same code, but their random angles will be different (since they're, um, random). The "Step size" tells the searches how far to move from their current location when they take a step; low values mean that not much of the space gets explored, high values mean that nearby solutions get missed. The "Desired fitness" slider tells the searches when to stop. Setting it low makes the search complete quickly, but with a poor solution; setting it high guarantees a better solution, but this may never be found. Welcome to the [halting problem](http://en.wikipedia.org/wiki/Halting_problem)!
+Click the box to start the search. The "Step size" tells the search how far to move from its current location taking a step; low values mean that not much of the space gets explored, high values mean that nearby solutions may get missed. The best solution found so far is highlighted in green. The search will keep going until you leave/reload the page.
 
 Whilst random walks are very simple search algorithms, their results can be pretty poor. Their lack of bias makes them easily applicable to many problems, but their repetition makes them not worth using ;)
--- js/optimisation/walk.js
+++ js/optimisation/walk.js
@@ -1,109 +1,129 @@
-window.walk_searchers = [];
-window.walk_lines = [];
-window.gradient = [];
+(function() {
+  var w          = $('#walk_playfield').width();
+  var h          = $('#walk_playfield').height();
+  var walk_lines = [];
+  var gradient   = [];
+  var best_fit   = 0;
+  var best_x     = 0;
+  var best_y     = 0;
+  var best;
+  var length;
 
-$(function() {
-	$('#walk_playfield').svg({onLoad: walk_init});
-	$('#walk_number').change(function(){
-		var new_n = Math.round($('#walk_number').val());
-		$('#walk_number_display').text(new_n+"");
-		while (new_n > window.walk_searchers.length) {
-			window.walk_searchers.push(1);
-			window.setTimeout(function(){
-				walk_step($('#walk_playfield').width() / 2, $('#walk_playfield').height() / 2, window.walk_searchers.length - 1);
-			}, 10);
-		}
-		while (new_n < window.walk_searchers.length) {
-			window.walk_searchers.pop();
-		}
-	});
-	$('#walk_number').change();
+  // Increase the fitness of some randomly chosen, overlapping areas
+  function walk_init(svg) {
+    _.times(Math.round(Math.random() * 100 + 1),
+                       function make_rect() {
+                         var l = Math.random();
+                         var r = Math.random();
+                         var t = Math.random();
+                         var b = Math.random();
+                         var temp;
+                         if (l > r) {
+                           temp = l;
+                           l    = r;
+                           r    = temp;
+                         }
+                         if (t > b) {
+                           temp = t;
+                           t = b;
+                           b = temp;
+                         }
+                         gradient.push(
+                           function(x, y, draw){
+                             if (draw) {
+                               $('#walk_playfield').svg('get')
+                                                   .rect(    l*w,     t*h,
+                                                         (r-l)*w, (b-t)*h,
+                                                         {'fill': 'white',
+                                                          'fill-opacity': 0.02});
+                             }
+                             else {
+                               if ((l < x / w) && (r > x / w) &&
+                                   (t < y / h) && (b > y / h)) {
+                                 return 1;
+                               }
+                               return 0;
+                             }
+                           });
+                       });
+    $('#walk_playfield').svg('get')
+                        .rect(0,
+                              0,
+                              $('#walk_playfield').width(),
+                              $('#walk_playfield').height(),
+                              {'fill': 'black'});
+      _.each(gradient, function(a){a(0, 0, true);});
+  };
 
-	$('#walk_fitness').change(function(){
-		$('#walk_fitness_display').text(Math.round($('#walk_fitness').val()));
-	});
-	$('#walk_fitness').change();
+  var fitn = _.memoize(function(x, y) {
+                         return _.reduce(_.map(gradient,
+                                               function(a) {
+                                                 return a(x, y, false);
+                                               }),
+                                         function(a,b) { return a + b; });
+                       });
 
-	$('#walk_step').change(function(){
-		$('#walk_step_display').text(Math.round($('#walk_step').val()));
-	});
-	$('#walk_step').change();
-});
+  function walk_step(x, y) {
+    var angle  = Math.random() * 2 * Math.PI;
+    var new_x  = x + (length * Math.cos(angle));
+    var new_y  = y + (length * Math.sin(angle));
+    if ((new_x < 0) || (new_x >= w) ||
+        (new_y < 0) || (new_y >= h)) {
+      // Wrap edges, but don't draw lines
+      if (new_x <  0) { new_x = new_x + w - 1; }
+      if (new_x >= w) { new_x = new_x - w + 1; }
+      if (new_y <  0) { new_y = new_y + h - 1; }
+      if (new_y >= h) { new_y = new_y - h + 1; }
+    }
+    else {
+      // No need to wrap; draw the line
+      walk_lines.push(
+        $('#walk_playfield').svg('get')
+                            .line(    x,     y,
+                                  new_x, new_y,
+                                  {'stroke-width': 1,
+                                   'stroke': 'white'}));
 
-walk_init = function(svg) {
-	_.times(Math.round(Math.random() * 100 + 1), function(){
-		var l = Math.random();
-		var r = Math.random();
-		var t = Math.random();
-		var b = Math.random();
-		var temp;
-		if (l > r) {
-			temp = l;
-			l = r;
-			r = temp;
-		}
-		if (t > b) {
-			temp = t;
-			t = b;
-			b = temp;
-		}
-		window.gradient.push(function(x, y, draw){
-			var w = $('#walk_playfield').width();
-			var h = $('#walk_playfield').height();
-			if (draw) {
-				$('#walk_playfield').svg('get').rect(l*w, t*h, (r-l)*w, (b-t)*h, {'fill': 'white', 'fill-opacity': 0.02});
-			}
-			else {
-				if ((l < x / w) && (r > x / w) && (t < y / h) && (b > y / h)) {
-					return 1;
-				}
-				return 0;
-			}
-		});
-	});
-	$('#walk_playfield').svg('get').rect(0,0,$('#walk_playfield').width(),$('#walk_playfield').height(),{'fill': 'black'});
-	_.each(window.gradient, function(a){a(0, 0, true);});
-}
+      // Find the fitness of this point
+      var fitness = fitn(new_x, new_y);
+      if (fitness >= best_fit) {
+        best_x   = new_x;
+        best_y   = new_y;
+        best_fit = fitness;
+        if (best) {
+          $('#walk_playfield').svg('get').remove(best);
+        }
+        best = $('#walk_playfield').svg('get')
+                                   .circle(best_x, best_y, 5, {fill: 'lime'});
+      }
 
-fitn = _.memoize(function(x, y) {
-	return _.reduce(_.map(window.gradient, function(a){ return a(x, y, false); }), function(a,b) { return a + b; });
-});
+      // Clean up old lines
+      while (walk_lines.length > 10) {
+        $('#walk_playfield').svg('get')
+                            .remove(walk_lines.shift());
+      }
+    }
+    next_step(new_x, new_y);
+  }
 
-walk_step = function(x, y, n) {
-	if (n + 1 > window.walk_searchers.length) {
-		while (window.walk_lines[n].length > 0) {
-			$('#walk_playfield').svg('get').remove(window.walk_lines[n].shift());
-		}
-		return;
-	}
-	var length = $('#walk_step').val();
-	var angle = Math.random() * 2 * Math.PI;
-	var new_x = x + (length * Math.cos(angle));
-	var new_y = y + (length * Math.sin(angle));
-	var w = $('#walk_playfield').width();
-	var h = $('#walk_playfield').height();
-	if ((new_x < 0) || (new_x >= w) || (new_y < 0) || (new_y >= h)) {
-		if (new_x < 0) { new_x = new_x + w - 1; }
-		if (new_x >= w) { new_x = new_x - w + 1; }
-		if (new_y < 0) { new_y = new_y + h - 1; }
-		if (new_y >= h) { new_y = new_y - h + 1; }
-	}
-	else {
-		while (window.walk_lines.length < n + 1) { window.walk_lines.push([]); }
-		var cols = ['white', 'yellow', 'lime', 'green', 'cyan', 'blue', 'purple'];
-		window.walk_lines[n].push($('#walk_playfield').svg('get').line(x, y, new_x, new_y, {stroke: cols[n % cols.length], 'stroke-width': 1}));
+  function next_step(x, y, n) {
+    setTimeout(function() {
+                 walk_step(x, y);
+               }, 10);
+  }
 
-		// Find the fitness of this point
-		var fitness = fitn(x, y);
-		if (fitness >= $('#walk_fitness').val()) {
-			new_x = x;
-			new_y = y;
-		}
+  // Initialise the demo on document.ready
+  $(function() {
+      $('#walk_playfield').svg({onLoad: walk_init});
+      $('#walk_playfield').click(function() {
+                                   length = $('#walk_step').val();
+                                   next_step(w / 2, h / 2);
+                                 });
 
-		// Clean up old lines
-		while (window.walk_lines[n].length > 10) {
-			$('#walk_playfield').svg('get').remove(window.walk_lines[n].shift());
-		}
-	}
-	window.setTimeout(function(){ walk_step(new_x, new_y, n); }, 10);
-}
+      $('#walk_step').change(function(){
+                               length = $('#walk_step').val();
+                               $('#walk_step_display').text(Math.round(length));
+                             });
+      $('#walk_step').change();
+    });
+})();
