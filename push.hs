3d7bc55a5525337a9c99bb71fb3271e53a8b2938
Chris Warburton <chriswarbo@gmail.com>
2014-10-29 09:25:31 +0000

    Fleshed-out push.hs

--- push.hs
+++ push.hs
@@ -1,34 +1,90 @@
 #!/usr/bin/env runhaskell
 
+import Control.Applicative
+import Data.List
 import System.Environment
 import System.Exit
+import System.IO
 import System.Process
 
-rebuild :: IO ()
-rebuild = exitOnFailure $ rawSystem "./site" ["rebuild"]
+-- rebuild
+
+rebuild :: IO ExitCode
+rebuild = rawSystem "./site" ["rebuild"]
+
+-- check
 
 watch :: IO ProcessHandle
-watch = runProcess "./site" ["watch"] Nothing Nothing Nothing Nothing Nothing
+watch = do dn <- devnull
+           p  <- runProcess "./site" ["watch"] Nothing
+                                               Nothing
+                                               Nothing
+                                               (Just dn)
+                                               Nothing
+           rawSystem "sleep" ["2"]
+           return p
 
-whileWatching x = do p <- watch
-                     x
+devnull :: IO Handle
+devnull = openFile "/dev/null" WriteMode
+
+whileWatching :: IO ExitCode -> IO ExitCode
+whileWatching x = do p    <- watch
+                     code <- x
                      terminateProcess p
+                     return code
 
-check = exitOnFailure $ runCommand "./404s.sh" >>= waitForProcess
+split404s :: [String] -> [String]
+split404s = let start x = "Found "       `isPrefixOf` x &&
+                          "broken links" `isInfixOf`  x
+                end   x = "FINISHED"     `isPrefixOf` x
+             in takeWhile (not . end) . dropWhile (not . start)
+{-
+split404s [] = []
+split404s xs = let last n = unlines . reverse . take n . reverse
+                in case span (not . ("404 Not Found" `isInfixOf`)) xs of
+                        (pre, post) -> last 2 pre : split404s (drop 1 post)
+-}
 
-runCheck :: IO ()
-runCheck = whileWatching check
+crawl :: IO String
+crawl = let f (ExitSuccess, _, _) = ""
+            f (_,           _, e) = e
+         in f <$> readProcessWithExitCode "wget"
+                                          ["-nd", "--spider", "-e",
+                                           "robots=off", "-L", "-r", "-p",
+                                           "http://localhost:8000"]
+                                          ""
 
-exitOnFailure x = do code <- x
-                     case code of
-                          ExitFailure _ -> exitWith code
-                          ExitSuccess   -> return ()
+check' :: IO [String]
+check' = split404s . lines <$> crawl
 
+
+check :: IO ExitCode
+check = whileWatching $ do fails <- check'
+                           putStr $ unlines fails
+                           return $ if null fails
+                                       then ExitSuccess
+                                       else ExitFailure 1
+
+-- scp
+
+scp :: IO ExitCode
 scp = rawSystem "scp" ["-r", "_site", "chriswarbo.net:~/"]
 
-optional = [("rebuild", rebuild), ("check", runCheck)]
+-- main
 
-toDo args = let opt = map snd $ filter ((`elem` args) . fst) optional
-             in opt ++ [exitOnFailure scp]
+exitOnFailure :: IO ExitCode -> IO ()
+exitOnFailure = let f ExitSuccess = return ()
+                    f code        = exitWith code
+                 in (>>= f)
 
-main = do fmap toDo getArgs >>= sequence
+ops :: [(String, IO ExitCode)]
+ops = [("rebuild", print "Rebuilding"     >> rebuild),
+       ("check",   print "Checking links" >> check  ),
+       ("copy",    print "Copying"        >> scp    )]
+
+op :: String -> IO ()
+op n = case lookup n ops of
+            Just x  -> exitOnFailure x
+            Nothing -> return ()
+
+main = getArgs >>= sequence . map op >> return ()
