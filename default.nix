7897acd5e1998ba768fed0d9b63f57fbbeb1dca3
Chris Warburton <chriswarbo@gmail.com>
2015-06-07 15:57:36 +0100

    Alter default.nix environment to work with haskell-ng

--- default.nix
+++ default.nix
@@ -1,23 +1,26 @@
 # Used by `nix-shell` to gather the dependencies for building this site
 with import <nixpkgs> {};
 
-stdenv.mkDerivation {
-  name = "chriswarbo.net";
+let env = haskellPackages.ghcWithPackages (p: with p; [
+            hakyll QuickCheck smallcheck
+          ]);
+in stdenv.mkDerivation {
+  name   = "chriswarbo.net";
+  src    = ./.;
 
-  src = ./.;
-
-  LANG = "en_US.UTF-8";
+  LANG           = "en_US.UTF-8";
   LOCALE_ARCHIVE = stdenv.lib.optionalString stdenv.isLinux "${glibcLocales}/lib/locale/locale-archive";
   CURL_CA_BUNDLE = "/etc/ssl/certs/ca-bundle.crt";
 
   buildInputs = [
+    env
     php
     git
     cacert
-    haskellPackages.ghc
-    haskellPackages.hakyll
-    haskellPackages.QuickCheck
-    haskellPackages.smallcheck
+    #haskellPackages.ghc
+    #haskellPackages.hakyll
+    #haskellPackages.QuickCheck
+    #haskellPackages.smallcheck
     gnuplot
     pngcrush
     imagemagick
@@ -27,6 +30,12 @@ stdenv.mkDerivation {
     utillinux
     time
     python
-    git2html
   ];
+
+  shellHook   = ''
+    export NIX_GHC="${env}/bin/ghc"
+    export NIX_GHCPKG="${env}/bin/ghc-pkg"
+    export NIX_GHC_DOCDIR="${env}/share/doc/ghc/html"
+    export NIX_GHC_LIBDIR=$( $NIX_GHC --print-libdir )
+  '';
 }
