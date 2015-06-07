3e4968626fa6f573aeec938ba0a917e2e0b5cd91
Chris Warburton <chriswarbo@gmail.com>
2015-06-07 17:11:38 +0100

    Link to repos

--- data/scripts/git2md_nojson.sh
+++ data/scripts/git2md_nojson.sh
@@ -2,17 +2,19 @@
 
 # Takes a git repo as an argument, spits out some stats and a README
 
-cd "root/git/$1.git"
+(cd "/home/chris/Programming/repos/$1.git"
 
-TICK='`'
-DATE=$(git log -n 1 --format=%ci)
+ TICK='`'
+ DATE=$(git log -n 1 --format=%ci)
 
-echo "*Last updated: $DATE*"
-echo
-echo "${TICK}git clone http://chriswarbo.net/git/$1.git${TICK}"
-echo
+ echo "*Last updated: $DATE*"
+ echo
+ echo "${TICK}git clone http://chriswarbo.net/git/$1.git${TICK}"
+ echo
+ echo "[View repository](/git/$1/)"
+ echo
 
-for f in "README"
-do
-    git show "master:$f"
-done
+ for f in "README"
+ do
+     git show "master:$f"
+ done)
