5d026af084c55e5e78e73d42e0f91733fbe30868
Chris Warburton <chriswarbo@gmail.com>
2015-06-16 16:46:22 +0100

    Remove comments

--- default.nix
+++ default.nix
@@ -8,19 +8,11 @@ in stdenv.mkDerivation {
   name   = "chriswarbo.net";
   src    = ./.;
 
-  LANG           = "en_US.UTF-8";
-  LOCALE_ARCHIVE = stdenv.lib.optionalString stdenv.isLinux "${glibcLocales}/lib/locale/locale-archive";
-  CURL_CA_BUNDLE = "/etc/ssl/certs/ca-bundle.crt";
-
   buildInputs = [
     env
     php
     git
     cacert
-    #haskellPackages.ghc
-    #haskellPackages.hakyll
-    #haskellPackages.QuickCheck
-    #haskellPackages.smallcheck
     gnuplot
     pngcrush
     imagemagick
