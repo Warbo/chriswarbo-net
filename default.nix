505315af44ac2f999d7af13bd6d82f1d85bd28f2
Chris Warburton <chriswarbo@gmail.com>
2015-09-03 09:53:17 +0100

    Remove some deps

--- default.nix
+++ default.nix
@@ -1,33 +1,17 @@
 # Used by `nix-shell` to gather the dependencies for building this site
 with import <nixpkgs> {};
 
-let env = haskellPackages.ghcWithPackages (p: with p; [
-            hakyll QuickCheck smallcheck
-          ]);
-in stdenv.mkDerivation {
+stdenv.mkDerivation {
   name   = "chriswarbo.net";
   src    = ./.;
 
   buildInputs = [
-    env
+    graphviz
+    xidel
     php
-    git
-    cacert
     gnuplot
-    pngcrush
-    imagemagick
-    coq
     panpipe
     panhandle
-    utillinux
-    time
     python
   ];
-
-  shellHook   = ''
-    export NIX_GHC="${env}/bin/ghc"
-    export NIX_GHCPKG="${env}/bin/ghc-pkg"
-    export NIX_GHC_DOCDIR="${env}/share/doc/ghc/html"
-    export NIX_GHC_LIBDIR=$( $NIX_GHC --print-libdir )
-  '';
 }
