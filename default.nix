488c69c574074533d892247d4130f57ed94dac8c
Chris Warburton <chriswarbo@gmail.com>
2017-02-28 02:21:07 +0000

    Pull out nix-shell shebang into rendering environment

--- default.nix
+++ default.nix
@@ -1,6 +1,21 @@
 # Used by `nix-shell` to gather the dependencies for building this site
 with import <nixpkgs> {};
-
+with {
+  stripper = stdenv.mkDerivation {
+    name         = "stripper";
+    buildInputs  = [ makeWrapper ];
+    script       = ./static/stripEmptyPreCode;
+    env          = buildEnv {
+      name  = "stripper-env";
+      paths = [ pythonPackages.python pythonPackages.beautifulsoup4 ];
+    };
+    buildCommand = ''
+      mkdir -p "$out/bin"
+      makeWrapper "$script" "$out/bin/stripEmptyPreCode" \
+        --prefix PATH : "$env/bin"
+    '';
+  };
+};
 stdenv.mkDerivation {
   name   = "chriswarbo.net";
   src    = ./.;
@@ -16,5 +31,6 @@ stdenv.mkDerivation {
     python
     pythonPackages.pyyaml
     pandoc
+    stripper
   ];
 }
