33edcae6f50213114cf5f9fb24916de306f0d885
Chris Warburton <chriswarbo@gmail.com>
2018-03-09 11:28:36 +0000

    Remove backporting which is no longer necessary

--- default.nix
+++ default.nix
@@ -1,10 +1,8 @@
 with import ./static/nix/defs.nix {};
 with configuredPkgs;
 with rec {
-  pages = callPackage ./pages.nix ({
-      inherit latestConfig pages repoSource;
-    } // (if python ? withPackages
-             then builtins.trace "No longer need python.withPackages check" {}
-             else { inherit (nixpkgs1709) python; }));
+  pages = callPackage ./pages.nix {
+    inherit latestConfig pages repoSource;
+  };
 };
 pages
