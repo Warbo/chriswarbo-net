6a59ff911c854bf13fa91327bc2951b2485b5901
Chris Warburton <chriswarbo@gmail.com>
2018-08-01 11:10:17 +0100

    Changes to commands.nix
    Including:
    
     - Use of withNix rather than trying to reinvent it (important for Nix 2.x)
     - Use of mkBin rather than trying to reinvent it
     - Addition of coqNoIde

--- static/nix/commands.nix
+++ static/nix/commands.nix
@@ -1,17 +1,15 @@
-{ attrsToDirs, buildEnv, dirContaining, haskellPackages, hfeed2atom, git,
-  glibcLocales, lib, libxslt, makeWrapper, mergeDirs, nix, pandocPkgs, python,
-  replace, runCommand, wget, wrap, xidel, xmlstarlet }:
+{ attrsToDirs, bash, buildEnv, commands, dirContaining, haskellPackages,
+  hfeed2atom, git, glibcLocales, lib, libxslt, mergeDirs, mkBin, pandocPkgs,
+  python, replace, runCommand, wget, withNix, wrap, xidel, xmlstarlet }:
 
 with builtins;
 with lib;
 with rec {
-  nixVars = {
+  nixVars = withNix {
     CALLING_NIX_RECURSIVELY = "1";
-    NIX_PATH   = getEnv "NIX_PATH";
-    NIX_REMOTE = "daemon";
-    REPO_REFS  = if getEnv "REPO_REFS" == ""
-                    then "{}"
-                    else getEnv "REPO_REFS";
+    REPO_REFS               = if getEnv "REPO_REFS" == ""
+                                 then "{}"
+                                 else getEnv "REPO_REFS";
   };
 
   bins = bin: attrsToDirs { inherit bin; };
@@ -23,98 +21,98 @@ with rec {
 
   bs = python.withPackages (p: [ p.python p.beautifulsoup4 ]);
 
-  commands = mapAttrs (n: v: bins {
-                        "${n}" = wrap (v // {
-                          paths = includingDeps (v.paths or []);
-                        });
-                      })
-                      (entries commands);
+  extras = {
+    # Avoids depending on GTK, Gnome, etc.
+    coqNoIde = coq.override { buildIde = false; };
 
-  entries = self: with self; {
-    cleanup = {
-      paths = [ stripEmptyPreCode summariseTables ];
-      file  = ../cleanup;
+    nix-instantiate = mkBin {
+      name   = "nix-instantiate";
+      paths  = [ bash ] ++ (withNix {}).buildInputs;
+      vars   = nixVars;
+      script = ''
+        #!/usr/bin/env bash
+        exec nix-instantiate "$@"
+      '';
     };
 
-    file2img = { file = ../file2img; };
-
-    git2md = {
-      paths = [ git wget ];
-      file  = ../git2md;
+    nix-shell = mkBin {
+      name   = "nix-shell";
+      paths  = [ bash ] ++ (withNix {}).buildInputs;
+      vars   = nixVars;
+      script = ''
+        #!/usr/bin/env bash
+        exec nix-shell "$@"
+      '';
     };
 
-    mkRedirectTo = {
-      vars = { TEMPLATE = ../redirectTemplate.html; };
-      file = ../mkRedirectTo;
-    };
-
-    mkRss = {
-      paths = [ libxslt ];
-      vars  = { XSL = ../atom2rss.xsl; };
-      file  = ../mkRss;
-    };
-
-    nix-instantiate = {
-      paths = [ nix ];
-      vars  = nixVars;
-      file  = "${nix}/bin/nix-instantiate";
-    };
-
-    nix-shell = {
-      paths = [ nix ];
-      vars  = nixVars;
-      file  = "${nix}/bin/nix-shell";
-    };
-
-    relativise = {
-      paths = [ xmlstarlet ];
-      file  = ../relativise;
-    };
-
-    relTo = {
-      paths = [ python ];
-      file  = ../relTo;
-    };
-
-    render_page = {
-      paths = [ cleanup pandocPkgs ];
-      vars  = {
-        defaultTemplate = ./templates/default.html;
-        LANG            = "en_US.UTF-8";
-        LOCALE_ARCHIVE  = "${glibcLocales}/lib/locale/locale-archive";
-      };
-      file  = ../render_page;
-    };
-
-    renderHcard = { file = ../renderHcard; };
-
-    showPost = {
-      paths = [ replace xidel ];
-      vars  = { RANTS = ../rants; };
-      file  = ../showPost;
-    };
-
-    showPosts = {
-      paths = [ showPost ];
-      file  = ../showPosts;
-    };
-
-    stripEmptyPreCode = {
-      paths = [ bs ];
-      file  = ../stripEmptyPreCode;
-    };
-
-    stripTitle = {
-      paths = [ bs ];
-      file  = ../stripTitle;
-    };
-
-    summariseTables = {
-      paths = [ bs ];
-      file  = ../summariseTables;
-    };
-
-    wrapCode = { file = ../wrapCode; };
   };
+
+  wrapScript = name: vals: mkBin (vals // {
+    inherit name;
+    file  = ./.. + "/${name}";
+    paths = includingDeps (vals.paths or []);
+  });
 };
-commands
+
+extras // mapAttrs wrapScript {
+  cleanup = {
+    paths = [ commands.stripEmptyPreCode commands.summariseTables ];
+  };
+
+  file2img = {};
+
+  git2md = {
+    paths = [ git wget ];
+  };
+
+  mkRedirectTo = {
+    vars = { TEMPLATE = ../redirectTemplate.html; };
+  };
+
+  mkRss = {
+    paths = [ libxslt ];
+    vars  = { XSL = ../atom2rss.xsl; };
+  };
+
+  relativise = {
+    paths = [ xmlstarlet ];
+  };
+
+  relTo = {
+    paths = [ python ];
+  };
+
+  render_page = {
+    paths = [ commands.cleanup pandocPkgs ];
+    vars  = {
+      defaultTemplate = ../../templates/default.html;
+      LANG            = "en_US.UTF-8";
+      LOCALE_ARCHIVE  = "${glibcLocales}/lib/locale/locale-archive";
+    };
+  };
+
+  renderHcard = {};
+
+  showPost = {
+    paths = [ replace xidel ];
+    vars  = { RANTS = ../rants; };
+  };
+
+  showPosts = {
+    paths = [ commands.showPost ];
+  };
+
+  stripEmptyPreCode = {
+    paths = [ bs ];
+  };
+
+  stripTitle = {
+    paths = [ bs ];
+  };
+
+  summariseTables = {
+    paths = [ bs ];
+  };
+
+  wrapCode = {};
+}
