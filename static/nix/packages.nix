52cf957f5f20703114e6733f2d35db7e4dcdca50
Chris Warburton <chriswarbo@gmail.com>
2020-07-25 22:31:32 +0100

    Bump deps

--- static/nix/packages.nix
+++ static/nix/packages.nix
@@ -5,6 +5,6 @@
 
 fetchgit {
   url    = "${repoSource}/warbo-packages.git";
-  rev    = "c472887";
-  sha256 = "0gjgimwhgg8q42bvswjnja1f1cg9akmi6j60s2czqlgqv15isjdj";
+  rev    = "e325b63";
+  sha256 = "1caa0747ss83sjpmbvsamsf9nibvjqhl9b94h7k1jxl7zkhj5x6z";
 }
