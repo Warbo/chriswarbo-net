ac55b2923ba676e99cabb5531e78ca3f254c08cd
Chris Warburton <chriswarbo@gmail.com>
2023-09-27 12:39:32 +0100

    Avoid hashing a fixed-output page's hash, and include tool paths

--- static/nix/render.nix
+++ static/nix/render.nix
@@ -5,7 +5,7 @@
 with rec {
   inherit (builtins) attrValues elem getAttr hasAttr hashFile map readFile;
   inherit (lib)
-    concatMapStringsSep concatStringsSep escapeShell hasSuffix mapAttrs
+    concatMapStringsSep concatStringsSep escapeShellArg hasSuffix mapAttrs
     optionalAttrs;
 
   test = { file, name }:
@@ -48,8 +48,24 @@ with rec {
   # If we're using a fixed-output derivation, its hash will be looked up in the
   # cache, even if we've changed the page. We would prefer such pages to be
   # rebuilt when changed, just in case their hash is out of date. To make this
-  # happen, we append a hash of the page's content to its derivation name.
-  prefix = if md ? sha256 then hashFile "sha256" file + "-" else "";
+  # happen, we append a hash of the page's content to its derivation name, so
+  # when the content changes, so does the name (and hence the store path).
+  prefix = with {
+    # When we hash the file, we need to strip out the `sha256:` line, so that
+    # when we plug in the correct hash, that won't cause the file's hash to
+    # change!
+    # We also add the paths to our processing commands, so any changes to those
+    # will cause rebuilds too.
+    # See https://stackoverflow.com/a/23696995/884682 for the awk command
+    stripHash = runCommand "strip-hash-${name}" { inherit file; } ''
+      {
+        echo ${commands.relativise}
+        echo ${commands.render_page}
+        awk '!/^sha256:/ || f++' < "$file"
+      } > "$out"
+    '';
+  };
+    if md ? sha256 then "${hashFile "sha256" "${stripHash}"}-" else "";
 
   untested = runCommand "untested-${prefix + name}" (vars // hash // {
     inherit dir file SOURCE_PATH TO_ROOT;
