045de196e508d5e545691fbc3363b7b110ef2b83
Chris Warburton <chriswarbo@gmail.com>
2023-09-29 14:17:14 +0100

    Bump warbo-packages for nix-helpers-source

--- static/nix/warbo-packages.nix
+++ static/nix/warbo-packages.nix
@@ -3,5 +3,5 @@ import (builtins.fetchGit {
   name = "warbo-packages";
   url = "http://chriswarbo.net/git/warbo-packages.git";
   ref = "master";
-  rev = "76c0beb850f96a7678b027a6e6eb79af35ab01d8";
+  rev = "6316535f0c9abddf2fa77c4f2861acc8b35375cb";
 }) { }
