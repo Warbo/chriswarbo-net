c86a97e55bb3cb25c777815e19ca2420ec3833dc
Chris Warburton <chriswarbo@gmail.com>
2023-10-25 14:37:53 +0100

    Hard-code list of repos, rather than looking them up impurely

--- static/nix/repos.nix
+++ static/nix/repos.nix
@@ -1,40 +1,10 @@
 # Give each git repo a page which redirects to the repo's own site
-{ attrsToDirs', isPath, jq, lib, redirect, render, repoSource, runCommand, wget
-, xidel }:
+{ attrsToDirs', lib, redirect, render, repoSource }:
 
 with builtins;
 with lib;
 with rec {
-  isAbsPath = p: isString p && p != "" && substring 0 1 p == "/";
-
-  dirIsRepo = n: v:
-    with {
-      isDir = v == "directory";
-      named = hasSuffix ".git" n;
-      hasDotGit = pathExists (repoSource + "/${n}/.git");
-    };
-    isDir && (named || hasDotGit);
-
-  # Fetch a list of repos from the online /git page. If we've opted to use a set
-  # of local clones, just list their directory (much faster!)
-  repoDirs = if isPath repoSource || isAbsPath repoSource then
-    attrNames (filterAttrs dirIsRepo (readDir repoSource))
-  else
-    import (runCommand "repoUrls.nix" {
-      inherit repoSource;
-      buildInputs = [ jq wget xidel ];
-    } ''
-      {
-        echo "["
-          wget -O- "$repoSource"   |
-            xidel - -e '//a/@href' |
-            grep '\.git'           |
-            jq -R '.'
-        echo "]"
-      } > "$out"
-    '');
-
-  repoNames = map (d: removeSuffix ".git" (baseNameOf d)) repoDirs;
+  inherit (repoSource) base repos;
 
   contents = listToAttrs (map (repo: rec {
     name = repo + ".html";
@@ -42,7 +12,7 @@ with rec {
       from = name;
       to = "../../git/" + repo;
     };
-  }) repoNames);
+  }) repos);
 };
 attrsToDirs' "repos" (contents // {
   "index.html" = render {
