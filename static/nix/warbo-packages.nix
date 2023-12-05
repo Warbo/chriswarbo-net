0ae084f7b50170f6dd7a279dd19cb5f9415aa8c8
Chris Warburton <chriswarbo@gmail.com>
2023-12-05 08:26:52 +0000

    warbo-packages: bump for nixpkgs2311 & panpipe/handle fix

--- static/nix/warbo-packages.nix
+++ static/nix/warbo-packages.nix
@@ -3,5 +3,5 @@ import (builtins.fetchGit {
   name = "warbo-packages";
   url = "http://chriswarbo.net/git/warbo-packages.git";
   ref = "master";
-  rev = "d0ef3f30de3b1476d368d26acdc9105ac3e3ca42";
+  rev = "acd6402cd0c764ae4233f11ecb56d2a3176cb01f";
 }) { }
