9485fc214b363842fef8206b75add2365745df9d
Chris Warburton <chriswarbo@gmail.com>
2023-12-11 16:37:05 +0000

    ivory: try to preserve UTF-8

--- projects/ivory/setup.sh
+++ projects/ivory/setup.sh
@@ -11,17 +11,20 @@ SHEBANG='#!'"$(command -v bash)"
 
 # Commands to use in our pipe attributes: both append code to the end of
 # $FILENAME # but one also shows it on the page and the other doesn't.
-printf '%s\ntee -a %s\necho >> %s\necho >> %s\n' \
+printf '%s\nexport LANG=en_US.UTF-8\ntee -a %s\necho >> %s\necho >> %s\n' \
        "$SHEBANG" "$FILENAME" "$FILENAME" "$FILENAME" > show
 
-printf '%s\ncat >> %s\necho >> %s\necho >> %s\n' \
+printf '%s\nexport LANG=en_US.UTF-8\ncat >> %s\necho >> %s\necho >> %s\n' \
        "$SHEBANG" "$FILENAME" "$FILENAME" "$FILENAME" > hide
 
 # Runs a test suite using Raco; prints out the code listing on failure (so we
 # can cross-reference line numbers from backtraces)
-printf '%s\nraco test %s || {\necho %s\ncat -n %s\necho %s\nfalse\n} 1>&2' \
-       "$SHEBANG" "$FILENAME" "BEGIN $FILENAME" "$FILENAME" "END $FILENAME" \
-       > tests
+{
+    printf '%s\nexport LANG=en_US.UTF-8\nraco test %s ' \
+           "$SHEBANG" "$FILENAME"
+    printf '|| {\necho %s\ncat -n %s\necho %s\nfalse\n} 1>&2' \
+           "BEGIN $FILENAME" "$FILENAME" "END $FILENAME"
+} > tests
 
 # Extracts a Base64-encoded file from a rendered Ivory page. This allows pages
 # to depend on the code from each other during rendering.
