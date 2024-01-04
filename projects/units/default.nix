f8c63546f0621dd0bdb1126d38a2a06a6e1d27b5
Chris Warburton <chriswarbo@gmail.com>
2024-01-04 10:59:47 +0000

    units: use a default.nix file

---
+++ projects/units/default.nix
@@ -0,0 +1,23 @@
+{ defs ? import ../../static/nix { }, nix-helpers ? defs.nix-helpers
+, nixpkgs-lib ? nix-helpers.nixpkgs-lib }:
+with rec {
+  inherit (defs) render;
+  inherit (nixpkgs-lib) mapAttrs mapAttrs';
+
+  renderPage = name: extraVars:
+    render {
+      name = "${name}.html";
+      file = ./. + "/${name}.md";
+      SOURCE_PATH = "projects/units/${name}.md";
+      TO_ROOT = "./../..";
+      vars = extraVars;
+    };
+
+  pages = mapAttrs (_: _: { }) (nix-helpers.suffixedFilesIn ".md" ./.);
+
+  allPages = mapAttrs renderPage (with allPages; pages);
+};
+mapAttrs' (name: value: {
+  inherit value;
+  name = "${name}.html";
+}) allPages
