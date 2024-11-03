1f7a4c647b912c92db2ec292eb955478f42d0e5f
Chris Warburton <chriswarbo@gmail.com>
2024-11-03 12:11:59 +0000

    nixfmt

--- projects/ivory/default.nix
+++ projects/ivory/default.nix
@@ -1,29 +1,46 @@
-{ defs ? import ../../static/nix { }, nix-helpers ? defs.nix-helpers
-, nixpkgs-lib ? nix-helpers.nixpkgs-lib }:
+{
+  defs ? import ../../static/nix { },
+  nix-helpers ? defs.nix-helpers,
+  nixpkgs-lib ? nix-helpers.nixpkgs-lib,
+}:
 with rec {
   inherit (defs) render;
   inherit (nixpkgs-lib) mapAttrs mapAttrs';
 
-  renderPage = name: extraVars:
+  renderPage =
+    name: extraVars:
     render {
       name = "${name}.html";
       file = ./. + "/${name}.md";
       SOURCE_PATH = "projects/ivory/${name}.md";
       TO_ROOT = "./../..";
-      vars = { setup = ./setup.sh; } // extraVars;
+      vars = {
+        setup = ./setup.sh;
+      } // extraVars;
     };
 
   pages = mapAttrs (_: _: { }) (nix-helpers.suffixedFilesIn ".md" ./.);
 
-  allPages = mapAttrs renderPage (with allPages;
-    pages // {
+  allPages = mapAttrs renderPage (
+    with allPages;
+    pages
+    // {
       geometric_algebra = {
-        inherit geometric_units numbers_in_scheme sums_and_products
-          zero_one_many;
+        inherit
+          geometric_units
+          numbers_in_scheme
+          sums_and_products
+          zero_one_many
+          ;
       };
-      sums_and_products = { inherit numbers_in_scheme; };
-      zero_one_many = { inherit numbers_in_scheme; };
-    });
+      sums_and_products = {
+        inherit numbers_in_scheme;
+      };
+      zero_one_many = {
+        inherit numbers_in_scheme;
+      };
+    }
+  );
 };
 mapAttrs' (name: value: {
   inherit value;
